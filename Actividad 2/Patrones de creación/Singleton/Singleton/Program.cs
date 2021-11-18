ad671d10615e0999190fc67b250c99fba457dc2b
Roger Castillo Chan <66329111+raccpunk@users.noreply.github.com>
2021-11-18 12:16:15 -0600

    add justificacion

--- Actividad 2/Patrones de creación/Singleton/Singleton/Program.cs
+++ Actividad 2/Patrones de creación/Singleton/Singleton/Program.cs
@@ -8,16 +8,25 @@ using System;
 
 namespace RefactoringGuru.DesignPatterns.Singleton.Conceptual.NonThreadSafe
 {
-    
-
-    
+    // La clase Singleton define el método `GetInstance` que sirve como
+    // alternativa al constructor y permite a los clientes acceder a la misma instancia de
+    // esta clase una y otra vez.
+    // ES: El Singleton siempre debe ser una clase 'sellada' para evitar la clase
+    // herencia a través de clases externas y también a través de clases anidadas.
     public sealed class Singleton
     {
+        // El constructor de Singleton siempre debe ser privado para evitar
+        // llamadas de construcción directas con el operador `new`.
         private Singleton() { }
-
-      
+        // La instancia de Singleton se almacena en un campo estático. Hay hay
+        // múltiples formas de inicializar este campo, todas tienen varios pros
+        // y contras. En este ejemplo, mostraremos la más simple de estas formas,
+        // que, sin embargo, no funciona muy bien en programas multiproceso.
         private static Singleton _instance;
-
+        // Este es el método estático que controla el acceso al singleton
+        // instancia. En la primera ejecución, crea un objeto singleton y coloca
+        // en el campo estático. En ejecuciones posteriores, devuelve el cliente.
+        // objeto existente almacenado en el campo estático.
         public static Singleton GetInstance()
         {
             if (_instance == null)
@@ -26,7 +35,8 @@ namespace RefactoringGuru.DesignPatterns.Singleton.Conceptual.NonThreadSafe
             }
             return _instance;
         }
-
+        // Finalmente, cualquier singleton debe definir alguna lógica de negocios, que puede
+        // se ejecutará en su instancia.
         public static void someBusinessLogic()
         {
            
@@ -37,10 +47,11 @@ namespace RefactoringGuru.DesignPatterns.Singleton.Conceptual.NonThreadSafe
     {
         static void Main(string[] args)
         {
+            // The client code.
             Singleton s1 = Singleton.GetInstance();
             Singleton s2 = Singleton.GetInstance();
 
-
+            //pide datos al usuario para la proxima validación
             Console.WriteLine("Type User");
             String UserId1 = Console.ReadLine();
                 Console.WriteLine("Type password");
@@ -48,7 +59,7 @@ namespace RefactoringGuru.DesignPatterns.Singleton.Conceptual.NonThreadSafe
 
                 String UserIdCorrect = "user";
                 String PassCorrect = "password";
-            
+            //se muestra la validación con un if and else para el mensaje que nos arrojara.
             if (s1 == s2 && UserId1 != UserIdCorrect && Pass != PassCorrect)
             {
 
