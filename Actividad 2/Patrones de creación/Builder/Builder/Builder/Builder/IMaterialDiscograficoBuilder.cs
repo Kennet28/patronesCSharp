415a60a339063a7bc74b59e5099112b3ab340cfe
Roger Castillo Chan <66329111+raccpunk@users.noreply.github.com>
2021-11-18 17:22:29 -0600

    add builder

---
+++ Actividad 2/Patrones de creación/Builder/Builder/Builder/Builder/IMaterialDiscograficoBuilder.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ConsoleApplication1.Entidades;
+
+namespace ConsoleApplication1.Builder
+{
+    public interface IMaterialDiscograficoBuilder
+    {
+        //uso de prefijos
+        MaterialDiscograficoBuilder ConNombre(String nombre);
+        MaterialDiscograficoBuilder TienePrecio(Double precio);
+        MaterialDiscograficoBuilder EnStock(int stock);
+
+        MaterialDiscograficoBuilder ComoGenero(Genero genero);
+        MaterialDiscograficoBuilder ComoAutor(Autor autor);
+        MaterialDiscograficoBuilder ComoTipoMaterialDiscografico(TipoMaterialDiscografico tipoMaterialDiscografico);
+        MaterialDiscograficoBuilder ComoCanciones(List<Cancion> canciones);
+    }
+}
